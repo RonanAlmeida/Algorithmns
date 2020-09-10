3f19c6a9baad740f75d820cdfa8df3b48bd57051
RonanAlmeida <ronancalmeida@hotmail.com>
2020-09-10 01:36:11 -0400

    fi b num

--- algo_questions.py
+++ algo_questions.py
@@ -1,4 +1,12 @@
-print("algos")
-print("algos")
-print("algos")
-print("algos")
+# Recursive Fibonacci function
+ def Fibonacci(n):
+    if n<=0:
+        print("DNE")
+    #  number is 0
+    elif n==1:
+        return 0
+    # number is 1
+    elif n==2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
