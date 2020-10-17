37471593346f45ed5ecf3bb5f7b67768818c4ba0
RonanAlmeida <ronancalmeida@hotmail.com>
2020-10-16 20:33:13 -0400

    testin

--- fib.py
+++ fib.py
@@ -27,7 +27,53 @@ def getNthFib(n):
 	# 5 [3,2]
 	# 6 [5,3]
 	# FIB-> 0 1 1 2 3 5 8 13
-	# N---> 1 2 3 4 5 6 7 8
+	# N---> 1 2 3 4 5 6 7 
+
+
+# n=5
+"""
+Odd: 5/2 = 2.5 -> round up 3
+    3/2  -> 1.5 -> round up 
+
+5 -> 3 -> 1 
+
+4 -> 2 -> 0
+AAAAA 5 
+ AAA  3
+  A   1
+
+AAAA
+ AA
+  
+"""
+def printthis(n):
+    # odd 
+    # 5 4 3 2 1
+    # 5  AAAAA
+    # 3  0AAA4    5 3 
+    # 1  01A34    5 1 0 1 2 3 4 
+
+
+    # AAAAAAA
+    # 0AAAAA1
+    #   AAA
+    #    A  01234
+    if n%2!=0:
+        nMAX=n
+        nSub =n
+        listN=['']*nMAX
+        while (nSub>0):
+
+            if (nSub%2!=0):
+                diff = nMAX-nSub
+                string = 'A'*nSub
+                spaces = diff 
+
+                
+                
+            nSub-=1
+    
+
 if __name__ == "__main__":
-    print(getNthFib(8))
+    print(printthis(5))
     # print(getNthFib(2))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+ if n%2!=0:
+        nMAX=n
+        nSub =n
+        while (nSub>0):
+
+            if (nSub%2!=0):
+                print('A'*0)
+                pass
+            nSub-=1
+    
